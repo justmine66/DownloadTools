7016422d502ce3da03d7fa6507fcfbb5dd059587
justmine <justmine@DESKTOP-UILU6SC>
2017-08-31 07:28:43 -0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DownloadTools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDownloadUri = new System.Windows.Forms.TextBox();
+            this.btnDirectDownload = new System.Windows.Forms.Button();
+            this.pbDownloadSpeed = new System.Windows.Forms.ProgressBar();
+            this.btnBreakpointResume = new System.Windows.Forms.Button();
+            this.lbSpreedMsg = new System.Windows.Forms.Label();
+            this.btnSliceDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "下载链接：";
+            // 
+            // tbDownloadUri
+            // 
+            this.tbDownloadUri.Location = new System.Drawing.Point(71, 15);
+            this.tbDownloadUri.Name = "tbDownloadUri";
+            this.tbDownloadUri.Size = new System.Drawing.Size(516, 20);
+            this.tbDownloadUri.TabIndex = 1;
+            this.tbDownloadUri.Text = "http://localhost/en_windows_10_enterprise_version_1607_updated_jul_2016_x64_dvd_9" +
+    "054264.zip";
+            // 
+            // btnDirectDownload
+            // 
+            this.btnDirectDownload.Location = new System.Drawing.Point(71, 53);
+            this.btnDirectDownload.Name = "btnDirectDownload";
+            this.btnDirectDownload.Size = new System.Drawing.Size(106, 25);
+            this.btnDirectDownload.TabIndex = 2;
+            this.btnDirectDownload.Text = "直接下载";
+            this.btnDirectDownload.UseVisualStyleBackColor = true;
+            this.btnDirectDownload.Click += new System.EventHandler(this.btnDirectDownload_Click);
+            // 
+            // pbDownloadSpeed
+            // 
+            this.pbDownloadSpeed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbDownloadSpeed.Location = new System.Drawing.Point(71, 102);
+            this.pbDownloadSpeed.Name = "pbDownloadSpeed";
+            this.pbDownloadSpeed.Size = new System.Drawing.Size(516, 25);
+            this.pbDownloadSpeed.TabIndex = 3;
+            // 
+            // btnBreakpointResume
+            // 
+            this.btnBreakpointResume.Location = new System.Drawing.Point(196, 53);
+            this.btnBreakpointResume.Name = "btnBreakpointResume";
+            this.btnBreakpointResume.Size = new System.Drawing.Size(106, 25);
+            this.btnBreakpointResume.TabIndex = 4;
+            this.btnBreakpointResume.Text = "断点续传";
+            this.btnBreakpointResume.UseVisualStyleBackColor = true;
+            this.btnBreakpointResume.Click += new System.EventHandler(this.btnBreakpointResume_Click);
+            // 
+            // lbSpreedMsg
+            // 
+            this.lbSpreedMsg.AutoSize = true;
+            this.lbSpreedMsg.Location = new System.Drawing.Point(71, 134);
+            this.lbSpreedMsg.Name = "lbSpreedMsg";
+            this.lbSpreedMsg.Size = new System.Drawing.Size(0, 13);
+            this.lbSpreedMsg.TabIndex = 5;
+            // 
+            // btnSliceDownload
+            // 
+            this.btnSliceDownload.Location = new System.Drawing.Point(318, 53);
+            this.btnSliceDownload.Name = "btnSliceDownload";
+            this.btnSliceDownload.Size = new System.Drawing.Size(106, 25);
+            this.btnSliceDownload.TabIndex = 6;
+            this.btnSliceDownload.Text = "分片下载";
+            this.btnSliceDownload.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 158);
+            this.Controls.Add(this.btnSliceDownload);
+            this.Controls.Add(this.lbSpreedMsg);
+            this.Controls.Add(this.btnBreakpointResume);
+            this.Controls.Add(this.pbDownloadSpeed);
+            this.Controls.Add(this.btnDirectDownload);
+            this.Controls.Add(this.tbDownloadUri);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "下载界面";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDownloadUri;
+        private System.Windows.Forms.Button btnDirectDownload;
+        private System.Windows.Forms.ProgressBar pbDownloadSpeed;
+        private System.Windows.Forms.Button btnBreakpointResume;
+        private System.Windows.Forms.Label lbSpreedMsg;
+        private System.Windows.Forms.Button btnSliceDownload;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DownloadTools
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDirectDownload_Click(object sender, EventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string downloadUri = tbDownloadUri.Text;
+                    var responseMsg = await httpClient.GetAsync(downloadUri, HttpCompletionOption.ResponseHeadersRead);
+                    using (var responseStream = await responseMsg.Content.ReadAsStreamAsync())
+                    {
+                        int readChunkSize = 1024 * 1024;//每次下载1M
+                        var buffer = new byte[readChunkSize];
+                        int writeChunkSize;
+                        long contentSize = responseMsg.Content.Headers.ContentLength.Value;
+                        var fileFullName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DateTime.Now.ToBinary() + ".zip");
+                        if (File.Exists(fileFullName)) File.Delete(fileFullName);
+                        int beginSecond = DateTime.Now.Second;
+                        long downloadedSize = 0;
+
+                        while ((writeChunkSize = await responseStream.ReadAsync(buffer, 0, readChunkSize)) > 0)
+                        {
+                            using (var fileStream = new FileStream(fileFullName, FileMode.Append, FileAccess.Write))
+                            {
+                                await fileStream.WriteAsync(buffer, 0, writeChunkSize);
+                            }
+
+                            downloadedSize += writeChunkSize;
+                            pbDownloadSpeed.Invoke((Action)(() =>
+                            {
+                                int downloadSpeed = Math.Max((int)(downloadedSize * 100 / contentSize), 1);
+                                this.pbDownloadNotificationInvoke(pbDownloadSpeed, downloadSpeed);
+                            }));
+
+                            int endSecond = DateTime.Now.Second;
+                            if (beginSecond != endSecond)
+                            {
+                                beginSecond = endSecond;
+                                lbSpreedMsg.Invoke((Action)(() =>
+                                {
+                                    string speedText = "下载速度: " + (downloadedSize / 1024) + " KB/S";
+                                    this.pbDownloadNotificationInvoke(lbSpreedMsg, speedText);
+                                }));
+                            }
+
+                            if (contentSize == downloadedSize)//下载完成
+                            {
+                                this.pbDownloadNotificationInvoke(pbDownloadSpeed, 100);
+                                this.pbDownloadNotificationInvoke(lbSpreedMsg, string.Empty);
+                                //未完善：验证文件是否与服务器一致
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        //是否暂停
+        static bool isPause = true;
+        //范围起始
+        static long rangeFrom;
+        //前一次文件实体标识
+        static string preETag;
+        //文件在服务器上的最后修改时间
+        static DateTimeOffset? preLastModified;
+        private async void btnBreakpointResume_Click(object sender, EventArgs e)
+        {
+            isPause = !isPause;
+            if (!isPause)
+            {
+                btnBreakpointResume.Text = "暂停";
+                Stopwatch stopwatch = null;
+                if (rangeFrom == 0)
+                {
+                    stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                }
+
+                await Task.Run(async () =>
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        string downloadUri = tbDownloadUri.Text;
+                        var requestMsg = new HttpRequestMessage(HttpMethod.Get, downloadUri);
+                        requestMsg.Headers.Range = new RangeHeaderValue(rangeFrom, null);
+                        var responseMsg = await httpClient.SendAsync(requestMsg, HttpCompletionOption.ResponseHeadersRead);
+
+                        if (!this.isAcceptRange(responseMsg) || this.isServerFileChanged(responseMsg))
+                            return;
+                        preETag = responseMsg.Headers.ETag.Tag;
+                        preLastModified = responseMsg.Content.Headers.LastModified;
+
+                        using (var responseStream = await responseMsg.Content.ReadAsStreamAsync())
+                        {
+                            int readChunkSize = 1024 * 1024;//每次下载1M
+                            var buffer = new byte[readChunkSize];
+                            int writeChunkSize;
+                            long contentSize = responseMsg.Content.Headers.ContentLength.Value;
+                            var fileFullName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "temp.zip");
+                            if (File.Exists(fileFullName)) File.Delete(fileFullName);
+                            int beginSecond = DateTime.Now.Second;
+
+                            while ((writeChunkSize = await responseStream.ReadAsync(buffer, 0, readChunkSize)) > 0 && !isPause)
+                            {
+                                using (var fileStream = new FileStream(fileFullName, FileMode.Append, FileAccess.Write))
+                                {
+                                    await fileStream.WriteAsync(buffer, 0, writeChunkSize);
+                                }
+
+                                rangeFrom += writeChunkSize;
+                                pbDownloadSpeed.Invoke((Action)(() =>
+                                {
+                                    int downloadSpeed = Math.Max((int)(rangeFrom * 100 / contentSize), 1);
+                                    this.pbDownloadNotificationInvoke(pbDownloadSpeed, downloadSpeed);
+                                }));
+
+                                int endSecond = DateTime.Now.Second;
+                                if (beginSecond != endSecond)
+                                {
+                                    beginSecond = endSecond;
+                                    lbSpreedMsg.Invoke((Action)(() =>
+                                    {
+                                        string speedText = "下载速度: " + (rangeFrom / 1024) + " KB/S";
+                                        this.pbDownloadNotificationInvoke(lbSpreedMsg, speedText);
+                                    }));
+                                }
+
+                                if (contentSize == rangeFrom)//下载完成
+                                {
+                                    this.pbDownloadNotificationInvoke(pbDownloadSpeed, 100);
+                                    if (stopwatch != null)
+                                    {
+                                        stopwatch.Stop();
+                                        string compMsg = "下载完成,耗时: " + stopwatch.Elapsed.Seconds + "秒,文件大小: " + rangeFrom / 1024 + " KB";
+                                        this.pbDownloadNotificationInvoke(lbSpreedMsg, compMsg);
+                                    }
+                                    //未完善：验证文件是否与服务器一致
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+            else
+            {
+                btnBreakpointResume.Text = "继续下载";
+                lbSpreedMsg.Text = string.Empty;
+            }
+        }
+
+        private void pbDownloadNotificationInvoke(Control control, object value)
+        {
+            control.Invoke((Action)(() =>
+            {
+                if (control is ProgressBar)
+                {
+                    int.TryParse(value + string.Empty, out int iValue);
+                    (control as ProgressBar).Value = Math.Min(iValue, 100);
+                }
+                if (control is Label)
+                {
+                    control.Text = value + string.Empty;
+                }
+            }));
+        }
+
+        //检查服务器是否支持断点续传
+        private bool isAcceptRange(HttpResponseMessage responseMessage)
+        {
+            //判断标准：当服务器不支持请求部分数据时，都会返回 Accept-Ranges: none。
+            if (responseMessage.Headers.AcceptRanges != null)
+            {
+                if (responseMessage.Headers.AcceptRanges.Contains("none"))
+                    return false;
+            }
+            return true;
+        }
+        //检查服务器端文件是否变化
+        private bool isServerFileChanged(HttpResponseMessage responseMessage)
+        {
+            if (preLastModified == null && string.IsNullOrEmpty(preETag))//第一次下载，文件视为未改变
+            {
+                return false;
+            }
+
+            //判断标准：ETag 和 Last-Modified都相同,则文件未改变。
+            var currETag = responseMessage.Headers.ETag.Tag;
+            DateTimeOffset? lastModified = responseMessage.Content.Headers.LastModified;
+            if (!string.IsNullOrWhiteSpace(currETag) && preETag == currETag
+                && lastModified != null && preLastModified == lastModified)
+            {
+                return false;//文件未改变，可以断点续传
+            }
+            return true;
+        }
+    }
+}
